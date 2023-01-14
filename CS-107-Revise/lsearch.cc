3232a52c5ab19380562a72b5ca4a2e1ffb89074e
Chandan Singh <Chandan.Singh@amd.com>
2023-01-14 10:44:24 +0530

    Seperated swap and lsearch into two differnt files

--- CS-107-Revise/lsearch.cc
+++ CS-107-Revise/lsearch.cc
@@ -1,86 +1,17 @@
-
 #include <stdio.h>
-#include <stdlib.h> //for malloc
-#include <string.h> //for memcpy
+#include <stdlib.h>
+#include <string.h>
 
-//Regular swap fuction for int 
-void int_swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int regular_swap_main()
-{
-    int a = 10, b = 20;
-    printf("A = %d B = %d \n",a,b);
-    int_swap(&a, &b);
-    printf("A = %d B = %d \n",a,b);
-    return 0;
-}
-
-
-// You are never supposed to derefrence the void* because its not aware of pointer arithmetic(no size info is available) unless you tell it
-void swap(void* vp1, void* vp2, int elem_size) {
-    char* buffer = (char*) malloc(sizeof(char)*elem_size);
-    memcpy(buffer, vp1, elem_size);
-    memcpy(vp1, vp2, elem_size);
-    memcpy(vp2, buffer, elem_size);
-    free(buffer);
-}
-
-// To test various different data types
-int generic_main() {
-    float a = 10.45, b = 20.08;
-    printf("A = %f B = %f \n",a,b);
-    swap(&a, &b, sizeof(float));
-    printf("A = %f B = %f \n",a,b);
-    return 0;
-}
-
-// How to swap a sting using a generic swap
-int sting_main() {
-    char* husband = _strdup("chandan");
-    char* wife = _strdup("jagruti");
-    // This does not work
-    //printf("husband = %s , wife = %s \n", husband, wife);
-    //swap(&husband, &wife, sizeof(*char));
-    //printf("husband = %s , wife = %s \n", husband, wife);
-
-    // Do not try to copy the contents of the char* (ie strings) just swap the pointers pointing
-    printf("husband = %s , wife = %s \n", husband, wife);
-    swap(&husband, &wife, sizeof(char*));
-    printf("husband = %s , wife = %s \n", husband, wife);
-    return 0;
+int int_lsearch(int arr[], int size, int key) {
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == key) {
+			return i;
+		}
+	}
+	return -1;
 }
 
 int main() {
-    
-    return 0;
+
+	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ CS-107-Revise/swap.cc
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <stdlib.h> //for malloc
+#include <string.h> //for memcpy
+
+//Regular swap fuction for int 
+void int_swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int regular_swap_main()
+{
+    int a = 10, b = 20;
+    printf("A = %d B = %d \n",a,b);
+    int_swap(&a, &b);
+    printf("A = %d B = %d \n",a,b);
+    return 0;
+}
+
+
+// You are never supposed to derefrence the void* because its not aware of pointer arithmetic(no size info is available) unless you tell it
+void swap(void* vp1, void* vp2, int elem_size) {
+    char* buffer = (char*) malloc(sizeof(char)*elem_size);
+    memcpy(buffer, vp1, elem_size);
+    memcpy(vp1, vp2, elem_size);
+    memcpy(vp2, buffer, elem_size);
+    free(buffer);
+}
+
+// To test various different data types
+int generic_main() {
+    float a = 10.45, b = 20.08;
+    printf("A = %f B = %f \n",a,b);
+    swap(&a, &b, sizeof(float));
+    printf("A = %f B = %f \n",a,b);
+    return 0;
+}
+
+// How to swap a sting using a generic swap
+int sting_main() {
+    char* husband = _strdup("chandan");
+    char* wife = _strdup("jagruti");
+    // This does not work
+    //printf("husband = %s , wife = %s \n", husband, wife);
+    //swap(&husband, &wife, sizeof(*char));
+    //printf("husband = %s , wife = %s \n", husband, wife);
+
+    // Do not try to copy the contents of the char* (ie strings) just swap the pointers pointing
+    printf("husband = %s , wife = %s \n", husband, wife);
+    swap(&husband, &wife, sizeof(char*));
+    printf("husband = %s , wife = %s \n", husband, wife);
+    return 0;
+}
